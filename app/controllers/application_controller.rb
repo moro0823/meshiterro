f82df67986f2452fad6ad926f03f6c1fe826989f
EC2 Default User <ec2-user@ip-172-31-61-221.ec2.internal>
2021-03-02 06:35:56 +0000

    start[4-5]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # devise_controller(ユーザー登録、ログイン認証機能)が使われる前に
+  # configure_permitted_parametersを実行する
   
   def after_sign_in_path_for(resorce)
     post_images_path
@@ -9,5 +11,8 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # configure_permitted_parametersを定義
+    # devise_parameter_sanitizerでnameのデータ操作を許可
+    #（ユーザー登録（sign_up）の際に、ユーザー名(name)が許可
   end
 end
